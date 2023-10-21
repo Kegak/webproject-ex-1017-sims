86330177b0ff64fca28f2092b88d89c8b35db918
Kegak <keeganjsims@gmail.com>
2023-10-20 20:25:05 -0500

    Fixed q1

--- simple-refactoring-examples/src/q1/extract_method/refactored/A.java
+++ simple-refactoring-examples/src/q1/extract_method/refactored/A.java
@@ -3,48 +3,45 @@ package q1.extract_method.refactored;
 import java.util.List;
 
 
+
 public class A {
    Node m1(List<Node> nodes, String p) {
-      // TODO: Your answer
-	   extractedMethodNode(nodes, p);
-      // other implementation
+      extractedMethod(nodes, p);
+      // other implementations
       return null;
    }
 
-   Edge m2(List<Edge> edgeList, String p) {
-      // TODO: Your answer
-	   extractedMethodEdge(edgeList, p);
-      // other implementation
+   Edge m2(List<Edge> edges, String p) {
+      extractedMethod(edges, p);
+      // other implementations
       return null;
    }
 
-   <T extends Node> void extractedMethodNode(List<T> objs, String p) {
-	      for (T obj : objs) {
-	         if (obj.contains(p))
-	            System.out.println(obj);
-	      }
-	   }
-   
-   <T extends Edge> void extractedMethodEdge(List<T> objs, String p) {
-	      for (T obj : objs) {
-	         if (obj.contains(p))
-	            System.out.println(obj);
-	      }
-	   }
+   /**
+    * Extract Method Refactoring by Type Parameterizing
+    */
+   <T extends Graph> void extractedMethod(List<T> objs, String p) {
+      for (T obj : objs) {
+         if (obj.contains(p))
+            System.out.println(obj);
+      }
+   }
 }
 
-class Node {
+/**
+ * Extract Superclass Refactoring
+ */
+class Graph {
    String name;
 
    public boolean contains(String p) {
       return name.contains(p);
    }
+
 }
 
-class Edge {
-   String name;
+class Node extends Graph {
+}
 
-   public boolean contains(String p) {
-       return name.contains(p);
-   }
+class Edge extends Graph {
 }
